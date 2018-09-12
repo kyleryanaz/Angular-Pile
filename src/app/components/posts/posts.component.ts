4c0f013958c35ea74107954a16cb6bc189bb9088
Kyle Ryan <kyle.ryan@truforcepest.com>
2018-09-12 12:12:25 -0700

    calling http in oninit instead of in constructor

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,46 +1,49 @@
 import { Http } from "@angular/http";
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "posts",
   templateUrl: "./posts.component.html",
   styleUrls: ["./posts.component.css"]
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
   posts: any[];
   private url = "http://jsonplaceholder.typicode.com/posts";
 
   constructor(private http: Http) {
-    http.get(this.url).subscribe(response => {
+  }
+
+  ngOnInit() {
+    this.http.get(this.url).subscribe(response => {
       this.posts = response.json();
     });
   }
 
-  createPost(input: HTMLInputElement) {
-    let post = { title: input.value };
-    input.value = "";
-    this.http.post(this.url, JSON.stringify(post)).subscribe(response => {
-      // console.log(response.json());
-      post["id"] = response.json().id;
-      this.posts.splice(0, 0, post);
-    });
-  }
+  // createPost(input: HTMLInputElement) {
+  //   let post = { title: input.value };
+  //   input.value = "";
+  //   this.http.post(this.url, JSON.stringify(post)).subscribe(response => {
+  //     // console.log(response.json());
+  //     post["id"] = response.json().id;
+  //     this.posts.splice(0, 0, post);
+  //   });
+  // }
 
-  updatePost(post) {
-    // Patch Method
-    this.http
-      .patch(this.url + "/" + post.id, JSON.stringify({ isRead: true }))
-      .subscribe(response => {
-        console.log(response.json());
-      });
-    // Put Method
-    // this.http.put(this.url, JSON.stringify(post))
-  }
+  // updatePost(post) {
+  //   // Patch Method
+  //   this.http
+  //     .patch(this.url + "/" + post.id, JSON.stringify({ isRead: true }))
+  //     .subscribe(response => {
+  //       console.log(response.json());
+  //     });
+  //   // Put Method
+  //   // this.http.put(this.url, JSON.stringify(post))
+  // }
 
-  deletePost(post) {
-    this.http.delete(this.url + "/" + post.id).subscribe(response => {
-      let index = this.posts.indexOf(post);
-      this.posts.splice(index, 1);
-    });
-  }
+  // deletePost(post) {
+  //   this.http.delete(this.url + "/" + post.id).subscribe(response => {
+  //     let index = this.posts.indexOf(post);
+  //     this.posts.splice(index, 1);
+  //   });
+  // }
 }
