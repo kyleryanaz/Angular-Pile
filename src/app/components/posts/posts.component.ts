f7a736e3b44ffb7943a4b4cd85b1e218afe05085
Kyle <kyleryan480@gmail.com>
2018-09-13 22:10:27 -0700

    this branch is misnamed: should be unexpected errors

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -9,38 +9,57 @@ import { Component, OnInit } from "@angular/core";
 export class PostsComponent implements OnInit {
   posts: any[];
 
-  constructor(private service: PostService) {
-  }
+  constructor(private service: PostService) {}
 
   ngOnInit() {
-    this.service.getPosts()
-    .subscribe(response => {
-      this.posts = response.json();
-    });
+    this.service.getPosts().subscribe(
+      response => {
+        this.posts = response.json();
+      },
+      error => {
+        alert("An unexpected error occured.");
+        console.log(error);
+      }
+    );
   }
 
   createPost(input: HTMLInputElement) {
     let post = { title: input.value };
     input.value = "";
-    this.service.createPost(post)
-    .subscribe(response => {
-      post["id"] = response.json().id;
-      this.posts.splice(0, 0, post);
-    });
+    this.service.createPost(post).subscribe(
+      response => {
+        post["id"] = response.json().id;
+        this.posts.splice(0, 0, post);
+      },
+      error => {
+        alert("An unexpected error occured.");
+        console.log(error);
+      }
+    );
   }
 
   updatePost(post) {
-    this.service.updatePost(post)
-      .subscribe(response => {
+    this.service.updatePost(post).subscribe(
+      response => {
         console.log(response.json());
-      });
+      },
+      error => {
+        alert("An unexpected error occured.");
+        console.log(error);
+      }
+    );
   }
 
   deletePost(post) {
-    this.service.deletePost(post.id)
-    .subscribe(response => {
-      let index = this.posts.indexOf(post);
-      this.posts.splice(index, 1);
-    });
+    this.service.deletePost(post.id).subscribe(
+      response => {
+        let index = this.posts.indexOf(post);
+        this.posts.splice(index, 1);
+      },
+      error => {
+        alert("An unexpected error occured.");
+        console.log(error);
+      }
+    );
   }
 }
